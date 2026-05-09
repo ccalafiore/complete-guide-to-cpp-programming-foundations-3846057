2a26363ca86a0a70fcdbb8ce06ab1b9716878927
Carmelo Calafiore <44749641+ccalafiore@users.noreply.github.com>
2026-05-09 09:42:52 +0100

    Update CodeDemo.cpp

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -5,10 +5,27 @@
 #include <iostream>
 #include <string>
 
-enum class character_role {protagonist, antagonist, sidekick, npc};
+enum class character_role { protagonist, antagonist, sidekick, npc, unkown };
+
+struct game_character
+{
+    std::string name = "";
+    int level = -1;
+    character_role role = character_role::unkown;
+};
 
 int main(){
+
+    game_character buddy;
     
+    buddy.name = "Tony";
+    buddy.level = 11;
+    buddy.role = character_role::sidekick;
+
+    std::cout << buddy.name << " is a character with role id " << (int) buddy.role << " at level " << buddy.level << std::endl;
+
+
     std::cout << std::endl << std::endl;
+
     return 0;
 }
