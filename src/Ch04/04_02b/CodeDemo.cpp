6d3581ad3d92595ba1b9ab06f8eae2e58c401ae5
Carmelo Calafiore <44749641+ccalafiore@users.noreply.github.com>
2026-06-27 11:52:32 +0100

    Update CodeDemo.cpp

--- src/Ch04/04_02b/CodeDemo.cpp
+++ src/Ch04/04_02b/CodeDemo.cpp
@@ -9,13 +9,29 @@ int main(){
     int a = 37;
     int *ptr;
 
-    ptr = nullptr;
+    ptr = &a;
 
-    std::cout << "             The value of a is " << ptr << std::endl;
+    std::cout << "             The value of a is " << a << std::endl;
     std::cout << "    ptr is pointing to address " << ptr << std::endl;
-    std::cout << "           The address of a is " << ptr << std::endl;
-    std::cout << "Where ptr is pointing, we have " << ptr << std::endl;
-    std::cout << "         The address of ptr is " << ptr << std::endl;
+    std::cout << "           The address of a is " << &a << std::endl;
+    std::cout << "Where ptr is pointing, we have " << *ptr << std::endl;
+    std::cout << "         The address of ptr is " << &ptr << std::endl;
+
+    *ptr = 10;
+
+    std::cout << "             The value of a is " << a << std::endl;
+    std::cout << "    ptr is pointing to address " << ptr << std::endl;
+    std::cout << "           The address of a is " << &a << std::endl;
+    std::cout << "Where ptr is pointing, we have " << *ptr << std::endl;
+    std::cout << "         The address of ptr is " << &ptr << std::endl;
+
+    a = 173;
+
+    std::cout << "             The value of a is " << a << std::endl;
+    std::cout << "    ptr is pointing to address " << ptr << std::endl;
+    std::cout << "           The address of a is " << &a << std::endl;
+    std::cout << "Where ptr is pointing, we have " << *ptr << std::endl;
+    std::cout << "         The address of ptr is " << &ptr << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
