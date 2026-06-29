fb14a988c5eaad24a289b356e845136de60b51a2
Carmelo Calafiore <44749641+ccalafiore@users.noreply.github.com>
2026-06-29 15:31:05 +0100

    Update CodeDemo.cpp

--- src/Ch04/04_03b/CodeDemo.cpp
+++ src/Ch04/04_03b/CodeDemo.cpp
@@ -6,9 +6,13 @@
 
 int main(){
     int highScores[] = {850, 745, 1220, 990};  // High scores in various game stages
-    int* scorePtr = highScores;   // Pointer initialized to the address of the array
+    int *scorePtr = highScores;   // Pointer initialized to the address of the array
 
-    std::cout << "Initial high score: " << *scorePtr << std::endl;  // Display the first high score using pointer
+    std::cout << "Initial high score using pointer: " << *scorePtr << std::endl;  // Display the first high score using pointer
+
+    std::cout << "Second high score using pointer: " << *(scorePtr + 2) << std::endl;  // Display the second high score using pointer
+
+    std::cout << "Third high score using array notation: " << scorePtr[3] << std::endl;  // Display the third high score using array notation
 
     std::cout << std::endl << std::endl;
     return 0;
