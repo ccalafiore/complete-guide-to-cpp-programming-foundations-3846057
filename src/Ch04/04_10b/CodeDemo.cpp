3b4b5b8e9d8861e167a0357c2651d51dc93ebfce
Carmelo Calafiore <44749641+ccalafiore@users.noreply.github.com>
2026-07-07 08:53:53 +0100

    Update CodeDemo.cpp

--- src/Ch04/04_10b/CodeDemo.cpp
+++ src/Ch04/04_10b/CodeDemo.cpp
@@ -3,26 +3,22 @@
 // The String Class, by Eduardo Corpeño 
 
 #include <iostream>
-#include <cstring>
+#include <string>
 
 int main(){
-    const size_t LENGTH = 50;
-    char racer1[] = "Speedy";
-    char racer2[LENGTH];
-    char raceResult[LENGTH * 2];
-
-    strncpy(racer2, "Lightning", sizeof(racer2) - 1);
-    racer2[sizeof(racer2) - 1] = '\0';
+    std::string racer1 = "Speedy";
+    std::string racer2 = "Lighting";
+    std::string *raceResult = nullptr;
 
     std::cout << "Racer 1: " << racer1 << std::endl;
     std::cout << "Racer 2: " << racer2 << std::endl;
 
-    strncpy(raceResult, "And the winner is ", sizeof(raceResult) - 1);
-    raceResult[sizeof(raceResult) - 1] = '\0';
+    raceResult = new std::string("And the winner is " + racer1);
 
-    strncat(raceResult, racer1, sizeof(raceResult) - strlen(raceResult) - 1);
+    std::cout << *raceResult << std::endl;
 
-    std::cout << raceResult << std::endl;
+    delete raceResult;
+    raceResult = nullptr;
 
     std::cout << std::endl << std::endl;
     return 0;
