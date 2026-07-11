d8f80dbb3b7ee1648d79b283938293860c6e737c
Carmelo Calafiore <44749641+ccalafiore@users.noreply.github.com>
2026-07-11 16:47:55 +0100

    Update CodeDemo.cpp

--- src/Ch05/05_01b/CodeDemo.cpp
+++ src/Ch05/05_01b/CodeDemo.cpp
@@ -8,6 +8,26 @@ int main(){
     int score = 1023;
     bool gamePaused = false;
     char action = 'x';
+
+    if (score > 1000)
+        std::cout << "Score is over 1000" << std::endl;
+
+    if (score % 2)
+        std::cout << "Score is odd" << std::endl;
+    else
+        std::cout << "Score is even" << std::endl;
+
+    std::cout << "Action is \"" << action << "\" is ";
+    if (action != 'w' && action != 'a' && action != 's' && action != 'd' &&
+        action != 'W' && action != 'A' && action != 'S' && action != 'D')
+        std::cout << "not ";
+    
+    std::cout << "a movement key." << std::endl;
+
+    if (gamePaused)
+        std::cout << "The game is paused!" << std::endl;
+    else
+        std::cout << "The game is running!" << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
