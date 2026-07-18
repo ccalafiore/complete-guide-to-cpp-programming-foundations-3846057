4d196cb8173c76e14c04446d7cb299c81a1b2229
Carmelo Calafiore <44749641+ccalafiore@users.noreply.github.com>
2026-07-18 14:12:44 +0100

    Update CodeDemo.cpp

--- src/Ch05/05_08b/CodeDemo.cpp
+++ src/Ch05/05_08b/CodeDemo.cpp
@@ -6,12 +6,18 @@
 #include <vector>
 
 int main(){
-    std::vector<int> lapTimes = {92, 85, 88, 90, 87};
+
+    using type_x = int;
+
+    std::vector<type_x> lapTimes = {92, 85, 88, 90, 87};
     float average;
 
     average = 0.0f;
-    for (int i = 0; i < lapTimes.size(); i++)
-        average += lapTimes[i];
+
+    // "int &x" makes a pointer of each element of lapTimes. Instead, "int x" makes a copy of each element.
+    for (type_x &x : lapTimes) {
+        average += x;
+    }
     average /= lapTimes.size();
     std::cout << "Average Lap Time: " << average << std::endl;
 
