8e7f0383f2138b90640a536d42a28151db6fed00
Carmelo Calafiore <44749641+ccalafiore@users.noreply.github.com>
2026-07-18 14:12:42 +0100

    Update CodeDemo.cpp

--- src/Ch05/05_06b/CodeDemo.cpp
+++ src/Ch05/05_06b/CodeDemo.cpp
@@ -7,7 +7,14 @@
 
 int main(){
     std::vector<int> lapTimes = {92, 85, 88, 90, 87};
-    float average;
+    float average = 0.0f;
+ 
+    for (int i = 0; i < lapTimes.size(); i += 1) {
+        average += lapTimes[i];
+    }
+    average /= lapTimes.size();
+
+    std::cout << "Average Lap Time: " << average << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
