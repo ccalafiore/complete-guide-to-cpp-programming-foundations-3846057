be09c91d4fa0fd3c6534cb11877166ab4aaaec0b
Carmelo Calafiore <44749641+ccalafiore@users.noreply.github.com>
2026-07-18 14:12:38 +0100

    Update CodeDemo.cpp

--- src/Ch05/05_04b/CodeDemo.cpp
+++ src/Ch05/05_04b/CodeDemo.cpp
@@ -9,6 +9,30 @@ int main(){
     std::vector<int> playerScores = {12, 25, 31, 47, 58};
     
     std::vector<int>::iterator scorePtr = playerScores.begin();
+
+    std::cout << "While loop:    ";
+
+    while (scorePtr != playerScores.end()) {
+
+        std::cout << *scorePtr << " ";
+
+        scorePtr = next(scorePtr, 1);
+
+    }
+    std::cout << std::endl;
+
+    std::cout << "Do-While loop: ";
+
+    int i = 0;
+    do {
+
+        std::cout << playerScores[i] << " ";
+        i += 1;
+
+    } while ( i < playerScores.size());
+
+
+    std::cout << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
